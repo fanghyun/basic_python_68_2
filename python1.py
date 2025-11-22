16dc58c51379ab8d7eb2ce70d9902ede86bb10fa
thanchanok <fang181047@gmail.com>
2025-11-22 13:29:22 +0700

    Variabal

---
+++ python1.py
@@ -0,0 +1,44 @@
+"""
+#
+# Part: python comment
+#
+"""
+
+"""
+v = s / t
+v = ความเร็ว (m/s)
+v = ระยะทาง (m)
+t = เวลา (s)
+"""
+
+print("Hello word!")
+"""
+#
+# part: Python Variable
+#
+"""
+x = 5
+y = "5"
+print(x,y)
+
+x = str(5)
+y = int(10)
+z = float(15)
+print(type(x), type(y), type(z))
+
+"""
+#
+# part: variable
+#
+"""
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myvar = "John"
+MYVAR = "John"
+my_var = "John"
+my_var = "John"
+# 2my_var = "John"
+# my-var = "John"
+# my var = "John"
