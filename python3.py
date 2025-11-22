8d40469cbdeafa77f38ab3f4e3e826197b35abd3
thanchanok <fang181047@gmail.com>
2025-11-22 14:32:21 +0700

    collection

---
+++ python3.py
@@ -0,0 +1,53 @@
+"""
+#
+# Part : collection of Python
+# List, Tuples, Sets, Dictionaries
+#
+"""
+
+"""
+#
+# List
+# 
+"""
+this_list = ["apple", "bannana", "cherry", "apple", "cherry"]
+print(this_list)
+print(this_list[1])
+print(type(this_list))
+print(len(this_list))
+
+"""
+#
+# Tuple
+# 
+"""
+this_tuple = ["apple", "bannana", "cherry", "apple", "cherry"]
+print(this_tuple)
+print(this_tuple[1])
+print(type(this_tuple))
+print(len(this_tuple))
+
+"""
+#
+# Set
+# 
+"""
+this_Set = ["apple", "bannana", "cherry", "apple", "cherry"]
+print(this_Set)
+print(type(this_Set))
+print(len(this_Set))
+
+"""
+#
+# Dictionaries
+# 
+"""
+this_dict = {
+    "name" : "John",
+    "age" : 36,
+    "country" : "Norway"
+}
+print(this_dict)
+print(this_dict["name"])
+print(type(this_dict))
+print(len(this_dict))
